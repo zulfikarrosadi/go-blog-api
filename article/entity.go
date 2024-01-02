3cc5beebf9b07c8f6517be23a806b73ff5089bc4
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-01-02 13:08:08 +0700

    add createArticleReqeust entity

--- article/entity.go
+++ article/entity.go
@@ -9,11 +9,17 @@ type Article struct {
 	Id        int            `json:"id"`
 	Title     string         `json:"title"`
 	Content   sql.NullString `json:"content"`
+	Author    int            `json:"author"`
 	CreatedAt time.Time      `json:"created_at"`
 }
 
 type ArticleRequest struct {
 	Id      string `param:"id"`
+	Title   string `json:"title" validate:"required"`
+	Content string `json:"content"`
+}
+
+type CreateArticleRequest struct {
 	Title   string `json:"title" validate:"required"`
 	Content string `json:"content"`
 }
