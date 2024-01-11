0d413026adc86c27e083db3026d45a14976984d3
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-01-11 08:13:32 +0700

    handle if slug is null and search the article based on articleId in query param

--- article/api.go
+++ article/api.go
@@ -2,11 +2,14 @@ package article
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/zulfikarrosadi/go-blog-api/auth"
+	"github.com/zulfikarrosadi/go-blog-api/web"
 )
 
 type ArticleApi interface {
@@ -33,15 +36,26 @@ func (aa *ArticleApiImpl) GetArticles(c echo.Context) error {
 }
 
 func (aa *ArticleApiImpl) GetArticleById(c echo.Context) error {
-	articleRequest := new(ArticleRequest)
-	c.Bind(articleRequest)
-	id, _ := strconv.Atoi(articleRequest.Id)
+	id, err := strconv.Atoi(c.QueryParam("id"))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, web.Response{
+			Status: "fail",
+			Code:   http.StatusNotFound,
+			Data:   nil,
+		})
+	}
+	s := c.Param("slug")
+	if trimed := strings.Trim(s, " "); len(trimed) < 1 || s == "" {
+		fmt.Println("slug is nil")
+		return c.JSON(http.StatusNotFound, web.Response{
+			Status: "fail",
+			Code:   http.StatusNotFound,
+			Data:   nil,
+		})
+	}
 
 	r := aa.ArticleServiceImpl.FindArticleById(id, c.Request().Context())
 
-	if r.Code == http.StatusNotFound {
-		return c.JSON(r.Code, r)
-	}
 	return c.JSON(r.Code, r)
 }
 
