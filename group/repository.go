d8713d4d8332ce95067f4fa618ef05ae2c33a4ce
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-01-18 21:12:15 +0700

    remove deleteGroup interface

--- group/repository.go
+++ group/repository.go
@@ -12,7 +12,6 @@ type GroupRepository interface {
 	CreateGroup(context.Context, *CreateGroupRequest) (int64, error)
 	GetGroup(context.Context) []Group
 	FindGroupById(context.Context, int64) (*Group, error)
-	DeleteGroup(context.Context, int64) error
 }
 
 type GroupRepositoryImpl struct {
