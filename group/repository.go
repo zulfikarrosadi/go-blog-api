1c76834e1bf761bc58f813d6ba090aa55ce96516
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-01-18 21:05:58 +0700

    add CreateGroup implementation

--- group/repository.go
+++ group/repository.go
@@ -3,6 +3,9 @@ package group
 import (
 	"context"
 	"database/sql"
+	"errors"
+
+	"github.com/zulfikarrosadi/go-blog-api/lib"
 )
 
 type GroupRepository interface {
@@ -14,4 +17,16 @@ type GroupRepository interface {
 
 type GroupRepositoryImpl struct {
 	*sql.DB
+}
+
+func (gri *GroupRepositoryImpl) CreateGroup(ctx context.Context, data *CreateGroupRequest) (int64, error) {
+	q := "INSERT INTO groups (title, description) VALUE (?,?)"
+	r, err := gri.DB.ExecContext(ctx, q, data.Title, data.Description)
+	if err != nil {
+		lib.ValidateErrorV2("create_group_repo", err)
+		return 0, errors.New("failed to create group, please try again")
+	}
+
+	i, _ := r.LastInsertId()
+	return i, nil
 }
