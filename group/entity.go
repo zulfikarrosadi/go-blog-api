701d34a622ea1cdbafca095b2587d92840b2a20e
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-01-18 21:18:15 +0700

    add createdBy field at group and createGroupRequest entity

--- group/entity.go
+++ group/entity.go
@@ -8,6 +8,7 @@ type Group struct {
 	Description    string    `json:"description"`
 	ProfilePicture string    `json:"profile_picture"`
 	CreatedAt      time.Time `json:"created_at"`
+	CreatedBy      int64     `json:"created_by"`
 }
 
 type CreateGroupRequest struct {
@@ -15,4 +16,5 @@ type CreateGroupRequest struct {
 	Description    string    `json:"description"`
 	ProfilePicture string    `json:"profile_picture"`
 	CreatedAt      time.Time `json:"created_at"`
+	CreatedBy      int64     `json:"created_by"`
 }
